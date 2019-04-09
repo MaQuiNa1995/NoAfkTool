0edd03208bee9ff54c5827a2e4fd29f4ad88ad64
MaQuiNa1995 <maquina1995@gmail.com>
2019-04-09 15:22:34 +0200

    Commit inicial del proyecto terminado

---
+++ src/main/java/es/maquina/no/afk/Main.java
@@ -0,0 +1,44 @@
+package es.maquina.no.afk;
+
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Robot;
+
+public class Main {
+	public static void main(String[] args) {
+		crearHilo().run();
+	}
+
+	private static Runnable crearHilo() {
+		Runnable hilo = (() -> {
+
+			Robot robot = null;
+
+			try {
+				robot = new Robot();
+			} catch (AWTException e) {
+				System.exit(-1);
+			}
+
+			do {
+
+				Point posicionRaton = MouseInfo.getPointerInfo().getLocation();
+
+				int x = posicionRaton.x + 1;
+				int y = posicionRaton.y + 1;
+
+				robot.mouseMove(x, y);
+
+				try {
+					Thread.currentThread().sleep(240 * 1000);
+				} catch (InterruptedException e) {
+				}
+
+			} while (Boolean.TRUE);
+
+		});
+
+		return new Thread(hilo);
+	}
+}
